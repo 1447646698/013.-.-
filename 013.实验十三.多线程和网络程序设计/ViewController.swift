39ecd148ade548f3161b70d9a8f13528823de51f
student <student@imac-145.local>
2018-12-17 17:20:41 +0800

    Initial Commit

---
+++ 013.实验十三.多线程和网络程序设计/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  013.实验十三.多线程和网络程序设计
+//
+//  Created by student on 2018/12/17.
+//  Copyright © 2018年 013.实验十三.多线程和网络程序设计. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
